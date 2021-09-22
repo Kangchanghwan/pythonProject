db336084289f0912848576eb5275e218503ee7e3
cokeBear <lgodl1598@naver.com>
2021-09-22 10:55:41 +0900

    첫날 파이선 연습.. 기초적인거라 더 해야할지 모르겠지만 그래도 몰랐던 것들이 하나씩 나오기 때문에 할만한거 같다..

--- main.py
+++ main.py
@@ -1,16 +1,127 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# print("hello") 코드업 예제 출력하기 01
 
+# print("Hello World")
+# print("Hello" ,"World") 코드업 예제 출력하기 02
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {"강창환"}')  # Press ⌘F8 to toggle the breakpoint.
-    print("maping11111234");
+# print("Hello")
+# print("World") 코드업 예제 출력하기 03
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# print("'Hello'") 코드업 예제 출력하기 04
 
-# See PyCharm heAlp at https://www.jetbrains.com/help/pycharm/
+# print('"Hello World"') 코드업 예제 출력하기 05
+
+# print("\"!@#$%^&*()\'") 코드업 예제 출력하기 06
+
+# print("\"C:\\Download\\\'hello'.py\"") 코드업 예제 출력하기 07
+
+# print("print(\"Hello\\nWorld\")") 코드업 예제 출력하기 08
+
+# c = input()
+# print(c) 문자 1개 입력받아 그대로 출력하기 09
+
+# n = input()
+# n = int(n)
+# print(n) 정수 1개 입력받아 int로 변환하여 출력하기 10
+
+# f = input()
+# f = float(f)
+# print(f) 실수 1개 입력받아 변환하여 출력하기 11
+
+# a = input()
+# b = input()
+# print(a)
+# print(b) 문자 2개 입력받아 줄을 바꿔 출력 12
+
+# f = input()
+# f = float(f)
+# print(f)
+# print(f)
+# print(f) 실수 1개 입력받아 3번 출력 13
+
+# a,b = input().split()
+# a = int(a)
+# b = int(b) split 사용시 공백을 기준으로 입력된 값들을 나누어 자른다.
+# print(a)
+# print(b) 정수 2개 입력받아 그대로 출력하기 14
+
+# a,b = input().split()
+# print(b , a) 문자 2개 입력받아 순서 바꿔 출력하기 15
+
+# a = input()
+# print(a,a,a) 문장 1개 입력받아 3번 출력하기 16
+
+# a,b = input().split(':')  ':' 기호를 기준으로 자른다
+# print (a,b,sep=':') ':'기호를 사이에 두고 값을 출력
+# 시간 입력받아 그대로 출력하기 17
+
+#연원일 입력받아 순서 바꿔 출력하기 18
+# a,b,c = input().split('.')
+# print(c,b,a,sep='-')
+
+#주만번호 입력받아 형태 바꿔 출력하기 19
+# a,b = input().split('-')
+# print(a,b,sep='')
+
+#단어 1개 입력받아 나누어 출력하기 20
+# a = input()
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+# print(a[5])
+
+# 연월일 입력받아 나누어 출력하기 21
+# a = input()
+# print(a[0:2],a[2:4],a[4:6])
+
+# 시분초 입력받아 분만 출력하기 22
+# a = input()
+# print(a[3:5])
+
+# 단어 2개 입력받아 이어 붙이기 23
+# a,b = input().split()
+# c = a + b
+# print(c)
+
+# 정수 2개 입력받아 합 계산하기 24
+# a,b = input().split()
+# c = int(a) + int(b)
+# print(c)
+
+#실수 2개 입력받아 합 계산하기 25
+# a,b = input().split()
+# c = float(a) + float(b)
+# print(c)
+
+# 10진 정수 입력받아 16진수로 출력하기 26
+# a = input()
+# n = int(a)
+# print('%x'%n)
+
+# 10진 정수 입력받아 16진수로 출력하기 27
+# a = input()
+# a = int(a)
+# print('%X'%a)
+
+# 16진 정수 입력받아 8진수로 출력하기 28
+# a = input()
+# a = int(a,16)
+# print('%o'%a)
+
+# 영문자 1개 입력받아 10진수로 변환하기 29
+# a = ord(input()) ord()는 어떤 문자의 순서위치 값을 의미
+# print(a)
+
+# 정수 입력받아 유니코드 문자로 변환하기 30
+# a = int(input())
+# print(chr(a))
+
+# 정수 입력받아 부호바꾸기 생략 31
+# 문자 1개 입력받아 다음 문자 출혁하기 생략 32
+# 정수 2개 입력받아 차 계산하기 생력 33
+# 실수 2개 입력받아 곱 계산하기 생략 34
+# 단어 여러번 출력하기 35
+# a,b = input().split()
+# print( a * int(b))
